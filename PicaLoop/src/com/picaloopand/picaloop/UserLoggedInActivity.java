019f0be0bcce23769b1a979a77d35c97b1e6c3cf
deepakdeodhar <deepakdeodhar26@gmail.com>
2014-12-11 18:57:13 -0500

    Missing fragments

---
+++ PicaLoop/src/com/picaloopand/picaloop/UserLoggedInActivity.java
@@ -0,0 +1,20 @@
+package com.picaloopand.picaloop;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserLoggedInActivity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_user_logged_in, container, false);
+    }
+}
