b6f94ab4130e1cd728cfad2b926eaeb6cce7034b
Deepak Deodhar <deepakdeodhar26@gmail.com>
2015-01-13 19:13:04 -0500

    New UI Changes

--- PicaLoop/src/com/picaloopand/picaloop/CreateALoopActivity.java
+++ PicaLoop/src/com/picaloopand/picaloop/CreateALoopActivity.java
@@ -1,5 +1,7 @@
 package com.picaloopand.picaloop;
 
+import android.R.integer;
+import android.R.string;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -28,9 +30,10 @@ public class CreateALoopActivity extends ActionBarActivity {
     public static final String TRUE = "TRUE";
     public static final String FALSE = "FALSE";
     
+    //Ordering constants
+    int counter = 1;
+    int drinkStatus = 0;
 
-    
-    
 	//Spot Icons
 	public ImageButton drinksButton ;
 	public ImageButton foodButton ;
@@ -41,7 +44,11 @@ public class CreateALoopActivity extends ActionBarActivity {
 	public ImageButton hotelButton ;
 	public ImageButton airportButton ;
 	public ImageButton photoButton ;
+	public ImageButton libraryButton ;
+	public ImageButton natureButton ;
+	public ImageButton paintingButton ;
 	public ImageButton spotSubmit ;
+
 	
 	//Display when selected
 	public ImageView drinksSelect ;
@@ -53,6 +60,9 @@ public class CreateALoopActivity extends ActionBarActivity {
 	public ImageView hotelSelect ;
 	public ImageView airportSelect ;
 	public ImageView photoSelect ;
+	public ImageView librarySelect ;
+	public ImageView natureSelect ;
+	public ImageView paintingSelect ;
 	protected MyApplication app;
 	
 	// Google client to interact with Google API
@@ -81,6 +91,9 @@ public class CreateALoopActivity extends ActionBarActivity {
         editSelectedSpots.putString("hotel", FALSE);
         editSelectedSpots.putString("airport", FALSE);
         editSelectedSpots.putString("photo", FALSE);
+        editSelectedSpots.putString("library", FALSE);
+        editSelectedSpots.putString("nature", FALSE);
+        editSelectedSpots.putString("painting", FALSE);
         editSelectedSpots.commit();
         
         // Get the application instance
@@ -103,6 +116,9 @@ public class CreateALoopActivity extends ActionBarActivity {
         hotelButton = (ImageButton) findViewById(R.id.hotelButton);
         airportButton = (ImageButton) findViewById(R.id.airportButton);
         photoButton = (ImageButton) findViewById(R.id.photoButton);
+        libraryButton = (ImageButton) findViewById(R.id.libraryButton);
+        natureButton = (ImageButton) findViewById(R.id.natureButton);
+        paintingButton = (ImageButton) findViewById(R.id.paintingButton);
         spotSubmit = (ImageButton) findViewById(R.id.spotSubmit);
         
         drinksSelect = (ImageView) findViewById(R.id.drinksSelect);
@@ -114,130 +130,217 @@ public class CreateALoopActivity extends ActionBarActivity {
         hotelSelect = (ImageView) findViewById(R.id.hotelSelect);
         airportSelect = (ImageView) findViewById(R.id.airportSelect);
         photoSelect = (ImageView) findViewById(R.id.photoSelect);
+        librarySelect = (ImageView) findViewById(R.id.librarySelect);
+        natureSelect = (ImageView) findViewById(R.id.natureSelect);
+        paintingSelect = (ImageView) findViewById(R.id.paintingSelect);
 
         drinksButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-            	  if (drinksSelect.getVisibility() == 0){
-            		  drinksSelect.setVisibility(View.GONE);
-                	  editSelectedSpots.putString("drinks", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  drinksSelect.setVisibility(View.VISIBLE);
-            	  editSelectedSpots.putString("drinks", TRUE);
-            	  editSelectedSpots.commit();
-            	  }
+            	  int black = R.drawable.ic_local_bar_black_48dp;
+            	  int grey = R.drawable.ic_local_bar_grey600_48dp;
+      		      pickCounter("drinks",drinksSelect,drinksButton,black,grey);
+            	  Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+               }
           });		
         foodButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-          	  if (foodSelect.getVisibility() == 0){
-        		  foodSelect.setVisibility(View.GONE);
-        		  editSelectedSpots.putString("food", FALSE);
-            	  editSelectedSpots.commit();
-        	  }else
+            	if (selectedSpots.getString("food", null) == FALSE){
         		  foodSelect.setVisibility(View.VISIBLE);
-        	      editSelectedSpots.putString("food", TRUE);
+        		  editSelectedSpots.putString("food", TRUE);
+            	  editSelectedSpots.commit();
+            	  counter++;
+        	  }else if (selectedSpots.getString("food", null) == TRUE){
+        		  foodSelect.setVisibility(View.GONE);
+        	      editSelectedSpots.putString("food", FALSE);
         	      editSelectedSpots.commit();
-            }
-          });	
-        movieButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            	  if (movieSelect.getVisibility() == 0){
-            		  movieSelect.setVisibility(View.GONE);
-            		  editSelectedSpots.putString("movie", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  movieSelect.setVisibility(View.VISIBLE);
-            	     editSelectedSpots.putString("movie", TRUE);
-            	     editSelectedSpots.commit();
+        	      counter--;
+                  }
+            Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
             }
           });	
         
-        bikeButton.setOnClickListener(new OnClickListener() {
+       movieButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-            	  if (bikeSelect.getVisibility() == 0){
-            		  bikeSelect.setVisibility(View.GONE);
-                	  editSelectedSpots.putString("bike", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  bikeSelect.setVisibility(View.VISIBLE);
-            	  editSelectedSpots.putString("bike", TRUE);
+            	if (selectedSpots.getString("movie", null) == FALSE){
+        		  movieSelect.setVisibility(View.VISIBLE);
+        		  editSelectedSpots.putString("movie", TRUE);
             	  editSelectedSpots.commit();
-            	  }
+            	  counter++;
+        	  }else if (selectedSpots.getString("movie", null) == TRUE){
+        		  movieSelect.setVisibility(View.GONE);
+        	      editSelectedSpots.putString("movie", FALSE);
+        	      editSelectedSpots.commit();
+        	      counter--;
+                  }
+            Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+            }
           });
         
-        groceryButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            	  if (grocerySelect.getVisibility() == 0){
-            		  grocerySelect.setVisibility(View.GONE);
-                	  editSelectedSpots.putString("grocery", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  grocerySelect.setVisibility(View.VISIBLE);
-            	  editSelectedSpots.putString("grocery", TRUE);
-            	  editSelectedSpots.commit();
-            	  }
-          });
+       bikeButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("bike", null) == FALSE){
+       		  bikeSelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("bike", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("bike", null) == TRUE){
+       		  bikeSelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("bike", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
         
-        waterButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            	  if (waterSelect.getVisibility() == 0){
-            		  waterSelect.setVisibility(View.GONE);
-                	  editSelectedSpots.putString("water", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  waterSelect.setVisibility(View.VISIBLE);
-            	  editSelectedSpots.putString("water", TRUE);
-            	  editSelectedSpots.commit();
-            	  }
-          });
+       groceryButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("grocery", null) == FALSE){
+       		  grocerySelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("grocery", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("grocery", null) == TRUE){
+       		  grocerySelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("grocery", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
         
-        hotelButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            	  if (hotelSelect.getVisibility() == 0){
-            		  hotelSelect.setVisibility(View.GONE);
-                	  editSelectedSpots.putString("hotel", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  hotelSelect.setVisibility(View.VISIBLE);
-            	  editSelectedSpots.putString("hotel", TRUE);
-            	  editSelectedSpots.commit();
-            	  }
-          });
+       waterButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("water", null) == FALSE){
+       		  waterSelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("water", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("water", null) == TRUE){
+       		  waterSelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("water", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
         
-        airportButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            	  if (airportSelect.getVisibility() == 0){
-            		  airportSelect.setVisibility(View.GONE);
-                	  editSelectedSpots.putString("airport", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  airportSelect.setVisibility(View.VISIBLE);
-            	  editSelectedSpots.putString("airport", TRUE);
-            	  editSelectedSpots.commit();
-            	  }
-          });
+       hotelButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("hotel", null) == FALSE){
+       		  hotelSelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("hotel", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("hotel", null) == TRUE){
+       		  hotelSelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("hotel", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
         
-        photoButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            	  if (photoSelect.getVisibility() == 0){
-            		  photoSelect.setVisibility(View.GONE);
-                	  editSelectedSpots.putString("photo", FALSE);
-                	  editSelectedSpots.commit();
-            	  }else
-            		  photoSelect.setVisibility(View.VISIBLE);
-            	  editSelectedSpots.putString("photo", TRUE);
-            	  editSelectedSpots.commit();
-            	  }
-          });
+       airportButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("airport", null) == FALSE){
+       		  airportSelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("airport", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("airport", null) == TRUE){
+       		  airportSelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("airport", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
+        
+       photoButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("photo", null) == FALSE){
+       		  photoSelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("photo", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("photo", null) == TRUE){
+       		  photoSelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("photo", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
+        
+       libraryButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("library", null) == FALSE){
+       		  librarySelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("library", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("library", null) == TRUE){
+       		  librarySelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("library", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
+        
+       natureButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("nature", null) == FALSE){
+       		  natureSelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("nature", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("nature", null) == TRUE){
+       		  natureSelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("nature", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
+        
+       paintingButton.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           	if (selectedSpots.getString("painting", null) == FALSE){
+       		  paintingSelect.setVisibility(View.VISIBLE);
+       		  editSelectedSpots.putString("painting", TRUE);
+           	  editSelectedSpots.commit();
+           	  counter++;
+       	  }else if (selectedSpots.getString("painting", null) == TRUE){
+       		  paintingSelect.setVisibility(View.GONE);
+       	      editSelectedSpots.putString("painting", FALSE);
+       	      editSelectedSpots.commit();
+       	      counter--;
+                 }
+           Toast.makeText(getApplicationContext(), Integer.toString(counter), Toast.LENGTH_LONG).show();
+           }
+         });
+        
       spotSubmit.setOnClickListener(new OnClickListener() {
           @Override
           public void onClick(View view) {
@@ -253,6 +356,24 @@ public class CreateALoopActivity extends ActionBarActivity {
 		startActivity(intent);
 	}
 	
+    private void pickCounter(String item,View view1,ImageButton view2,int black,int grey){
+
+        if (selectedSpots.getString(item, null) == FALSE){
+     		editSelectedSpots.putString(item, TRUE);
+           	editSelectedSpots.commit();
+           	view1.setVisibility(View.VISIBLE);
+           	view2.setImageResource(grey);
+           	counter++;
+        }else if (selectedSpots.getString(item, null) == TRUE){
+   		  editSelectedSpots.putString(item, FALSE);
+       	  editSelectedSpots.commit();
+       	  view1.setVisibility(View.GONE);
+       	  view2.setImageResource(black);
+        	counter--;
+        }
+        	
+    }
+    
     @Override
 	 public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
